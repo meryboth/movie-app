a383014fe5a342db32de6e87885a8bc225e8f402
Marilyn Botheatoz <65403789+meryboth@users.noreply.github.com>
2022-01-13 12:51:35 -0300

    update desktop visualization

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,7 +9,7 @@ export default function Card({movie}){
                 <h3 className="card--title">{movie.title}</h3>
                 <p><small>Release date: {movie.release_date}</small></p>
                 <p><small>Rating: {movie.vote_average}</small></p>
-                <p className="card--desc">{movie.overview}</p>
+                {/* <p className="card--desc">{movie.overview}</p> */}
             </div>
         </div>
     )
